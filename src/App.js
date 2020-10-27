25a1f5e44e83f0bb12bdea4fea245a0f2b7f4f50
lamoureux-sylvain <lamoureux.sylvain@gmail.com>
2020-10-27 13:06:56 +0100

    ajout des props au composant contact

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact
+        name="Mike Oxlong"
+        avatar="https://randomuser.me/api/portraits/men/58.jpg"
+        online
+      />
+      <Contact
+        name="Phill McHunt"
+        avatar="https://randomuser.me/api/portraits/men/18.jpg"
+
+      />
+      <Contact
+        name="E. Norma Stits"
+        avatar="https://randomuser.me/api/portraits/women/10.jpg"
+        online
+      />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,49 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import "./Contact.css";
 
-function Contact() {
+// function Contact() {
+//     return (
+//         <div className="Contact">
+//             <img
+//                 className="avatar"
+//                 src="https://randomuser.me/api/portraits/men/58.jpg"
+//                 alt="Mike Oxlong"
+//             />
+//             <div>
+//                 <p className="name">Mike Oxlong</p>
+//                 <div className="status">
+//                     <div className="status-online" />
+//                     <p className="status-text">online</p>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+function Contact(props) {
     return (
         <div className="Contact">
             <img
                 className="avatar"
-                src="https://randomuser.me/api/portraits/men/58.jpg"
-                alt="Mike Oxlong"
+                src={props.avatar}
+                alt={props.name}
             />
             <div>
-                <p className="name">Mike Oxlong</p>
+                <p className="name">{props.name}</p>
                 <div className="status">
-                    <div className="status-online" />
-                    <p className="status-text">online</p>
+                    <div className={props.online ? 'status-online' : 'status-offline'} />
+                    <p className="status-text">{props.online ? 'online' : 'offline'}</p>
                 </div>
             </div>
         </div>
     );
 }
+
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    online: PropTypes.bool.isRequired,
+};
 
 export default Contact;
