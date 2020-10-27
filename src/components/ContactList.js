9235aab334b6eacd52c56207955e1a4f3a4becca
lamoureux-sylvain <lamoureux.sylvain@gmail.com>
2020-10-27 13:29:30 +0100

    creation du tableau users

---
+++ src/components/ContactList.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const users = [
+    {
+        name: 'Mike Coxlong',
+        avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
+        online: false
+    },
+    {
+        name: 'Maya Normusbutt',
+        avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
+        online: true
+    },
+    {
+        name: 'Mike Hawk',
+        avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
+        online: true
+    },
+    {
+        name: 'E. Norma Stits',
+        avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
+        online: false
+    },
+    {
+        name: 'Ho Lee Fock',
+        avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
+        online: true
+    }
+];
